4c7cf9874403bd7d43bbf2ed86c6f3f1cfa44308
Varun <varun@innavoto.com>
2025-11-10 20:05:18 +0530

    created library for logger; created custom logger;

---
+++ src/include/logger.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define LOG_ERROR(fmt, ...) log_message(LOG_LEVEL_ERROR, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...) log_message(LOG_LEVEL_WARN, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...) log_message(LOG_LEVEL_INFO, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+
+// Optional: Use conditional compilation to eliminate high-volume debug logs in production
+#ifdef COMPILER_DEBUG_BUILD
+#define LOG_DEBUG(fmt, ...) log_message(LOG_LEVEL_DEBUG, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+#define LOG_TRACE(fmt, ...) log_message(LOG_LEVEL_TRACE, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+#else
+#define LOG_DEBUG(fmt, ...) (void)0
+#define LOG_TRACE(fmt, ...) (void)0
+#endif
+
+typedef enum { LOG_LEVEL_ERROR = 1, LOG_LEVEL_WARN, LOG_LEVEL_INFO, LOG_LEVEL_DEBUG, LOG_LEVEL_TRACE } LogLevel;
+
+extern LogLevel g_compiler_log_level;
+
+void log_message(LogLevel level, const char* file, int line, const char* func, const char* fmt, ...);
---
+++ src/logger.c
@@ -0,0 +1,45 @@
+#include "logger.h"
+#include <stdarg.h>
+
+// Set your default level here. E.g., INFO shows errors, warnings, and general info.
+LogLevel g_compiler_log_level = LOG_LEVEL_DEBUG;
+
+void log_message(LogLevel level, const char* file, int line, const char* func, const char* fmt,
+                 ...) {
+    if (level > g_compiler_log_level) {
+        return;
+    }
+
+    // Determine the level string
+    const char* level_str;
+    switch (level) {
+    case LOG_LEVEL_ERROR:
+        level_str = "ERROR";
+        break;
+    case LOG_LEVEL_WARN:
+        level_str = "WARN ";
+        break;
+    case LOG_LEVEL_INFO:
+        level_str = "INFO ";
+        break;
+    case LOG_LEVEL_DEBUG:
+        level_str = "DEBUG";
+        break;
+    case LOG_LEVEL_TRACE:
+        level_str = "TRACE";
+        break;
+    default:
+        return;
+    }
+
+    // Print Context to stderr (Standard Error)
+    fprintf(stderr, "[%s] %s:%d %s() - ", level_str, file, line, func);
+
+    // Print the actual log message using vfprintf
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+
+    fprintf(stderr, "\n");
+}
